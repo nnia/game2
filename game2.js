7b5b433db7d9dce110d7ceff5deb5918fd35cda7
nnia <nnia@mail.ru>
2026-04-02 20:40:42 +0300

    Update game2.js

--- game2.js
+++ game2.js
@@ -1,6 +1,9 @@
 
 var cvs = document.getElementById("gameCanvas");
+
 var ctx = cvs.getContext("2d");
+cvs.width = window.innerWidth;
+cvs.height = window.innerHeight;
 
 var box = new Image();
 box.src = "i1.png";
@@ -15,11 +18,16 @@ var red = 240;
 var green = 240;
 var blue = 255;
 
-ctx.fillStyle = "#d0e0f0"; // Цвет фона
-ctx.fillRect(0, 0, 1200, 600);
+var komplekt = 19;
 
-ctx.font = "16px comic";
-ctx.strokeText("Hello!", 580, 100);
+ctx.fillStyle = "#d0e0f0"; // Цвет фона
+
+
+window.addEventListener("resize", () => {
+  cvs.width = window.innerWidth;
+  cvs.height = window.innerHeight;
+}, true);
+
 
 document.addEventListener("mouseup", function (e) {
     
@@ -54,40 +62,63 @@ let cright = ["1", "0"];
 let cexist = [1, 1];
 let cx = [100, 300];
 let cy = [150, 50];
+var N = 2;
+var exist = N;
 
 function draw() {
 
     ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-    ctx.fillRect(0, 0, 1200, 600);
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-    if (boxX < 0) boxX = 0;
-    if (boxX > 1136) boxX = 1136;
-    if (boxY < 0) boxY = 0;
-    if (boxY > 500) boxY = 500;
+    ctx.font = "24px comic";
+    if (komplekt == 19) ctx.strokeText("Ловите изделия для 19 к., пока не стемнело", 16, 16);
+    if (komplekt == 16) ctx.strokeText("Ловите изделия для 16 к., пока не стемнело", 16, 16);
+    ctx.font = "20px comic";
+
+    // ящик
     ctx.drawImage(box, boxX, boxY);
 
+    // сумерки
     if (red > 100) red -=0.1;
     if (green > 100) green -=0.1;
     if (blue > 100) blue -= 0.1;
-
     if (blue < 160)
     {
-      ctx.font = "16px comic";
-      ctx.strokeText("Стемнело, но можно продолжать", boxX, boxY+70);
+        ctx.strokeText("Стемнело, но можно продолжать", boxX, boxY+110);
     }
 
-    for (var i=0; i < 2; i++) 
+    for (var i=0; i < N; i++) 
     {
        if (cexist[i] == 1)
        { 
-           ctx.font = "16px comic";
+           // вывод
            ctx.strokeText(ctext[i], cx[i], cy[i]);
            cy[i] ++;
-           if (cy[i] > 400) cy[i] = 0;
-       }
+           if (cy[i] > window.innerHeight) cy[i] = 32;
+
+           if ((cx[i] > boxX - 112) && (cx[i] < boxX + 112) &&
+               (cy[i] > boxY + 20) && (cy[i] < boxY + 40))
+           {
+                cexist[i] = 0;
+	        exist--;
+                red+=10; green+=10; blue+=10;
+           }
+        }
     } 
+    if (exist == 0)
+    {  
+       exist = N;
+
+       komplekt = 35 - komplekt;
+       for (var i=0; i < N; i++) 
+       {
+           cexist[i] = 1;
+           if ((cy[i] > boxY + 20) && (cy[i] < boxY + 40)) cy[i] -= 80;
+       } 
+
+    }
  
-    requestAnimationFrame(draw); 
+    requestAnimationFrame(draw); // Вызов функции постоянно
 }
 
 box.onload = draw;
